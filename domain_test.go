c9896c0c26b991162808197faee65a145631940d
v.burygin <HeccrfzPtvkz>
2022-01-11 23:36:25 +0300

    FEATURE TG-15541 add multicreate domain thin

--- domain_test.go
+++ domain_test.go
@@ -3,6 +3,8 @@ package veil_api_client_go
 import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"math/rand"
 	"testing"
 )
 
@@ -79,6 +81,59 @@ func Test_DomainUpdate(t *testing.T) {
 func Test_DomainRemove(t *testing.T) {
 	client := NewClient("", "", false)
 
+	status, _, err := client.Domain.Remove(TestDomainID)
+	assert.Nil(t, err)
+	assert.True(t, status)
+
+	return
+}
+
+func Test_DomainMultiCreate(t *testing.T) {
+	client := NewClient("", "", false)
+
+	nodesResponse, _, err := client.Node.List()
+	require.Nil(t, err, err)
+	if len(nodesResponse.Results) == 0 {
+		t.SkipNow()
+	}
+	randomNode := nodesResponse.Results[rand.Intn(len(nodesResponse.Results))]
+
+	config := new(DomainMultiCreateConfig)
+	config.DomainId = TestDomainID
+	config.VerboseName = TestDomainName
+	config.Node = randomNode.Id
+	domain, _, err := client.Domain.MultiCreate(*config)
+	require.Nil(t, err, err)
+	assert.NotEqual(t, domain.Id, "", "Domain Id can not be empty")
+	status, _, err := client.Domain.Remove(TestDomainID)
+	assert.Nil(t, err)
+	assert.True(t, status)
+
+	return
+}
+
+func Test_DomainMultiCreateThin(t *testing.T) {
+	client := NewClient("", "", false)
+
+	templatesResponse, _, err := client.Domain.ListParams(map[string]string{
+		"template": "true",
+		"status":   "ACTIVE",
+	})
+	require.Nil(t, err, err)
+	if len(templatesResponse.Results) == 0 {
+		t.SkipNow()
+	}
+	randomTemplate := templatesResponse.Results[rand.Intn(len(templatesResponse.Results))]
+
+	config := new(DomainMultiCreateConfig)
+	config.DomainId = TestDomainID
+	config.VerboseName = TestDomainName
+	config.Parent = randomTemplate.Id
+	config.Thin = true
+	config.StartOn = true
+	domain, _, err := client.Domain.MultiCreate(*config)
+	require.Nil(t, err, err)
+	assert.NotEqual(t, domain.Id, "", "Domain Id can not be empty")
 	status, _, err := client.Domain.Remove(TestDomainID)
 	assert.Nil(t, err)
 	assert.True(t, status)
